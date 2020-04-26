99c6726a96e11a0282f6a5dfb3c74e9adf35e189
Divya Varun <divyavarun85@gmail.com>
2020-04-26 00:37:57 -0400

    added POST functionality

--- server.js
+++ server.js
@@ -1,19 +1,53 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
+const projectData = {};
+/* Fetchn */
+const fetch = require("node-fetch");
 // Require Express to run server and routes
+const express = require('express')
 
 // Start up an instance of app
+const app = express()
 
 /* Middleware*/
+const bodyParser = require('body-parser');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8080;
+app.listen(port,listening);
+function listening(){
+   
+console.log("server running");
+console.log('Express listening on port', this.address().port);
+}
+
+
+
+const weatherDetails =[];
+app.post('/weatherDetails',processWeather)
+
+function processWeather(req,res) {
+    //console.log(req.body);
+
+    newDetailsEntry = {
+        Zip :req.body.Zip,
+        userFeelings : req.body.userFeelings,
+        Date:req.body.Date,
+        Temperature :req.body.Temperature,
+        userResponse :req.body.userResponse
+    }
+    weatherDetails.push(newDetailsEntry);
+    res.send(weatherDetails);
+    console.log(weatherDetails);
+}
--- website/app.js
+++ website/app.js
@@ -1,7 +1,49 @@
 /* Global Variables */
 
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // LEarning git//
+
+
+
+    /*POST DATA*/
+    const postData = async ( url = '', data = {})=>{
+         console.log(data)
+   
+         const response = await fetch('http://localhost:8080/weatherDetails' ,{
+         method: 'POST', // *GET, POST, PUT, DELETE, etc.
+         credentials: 'same-origin', // include, *same-origin, omit
+         headers: {
+             'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(data), // body data type must match "Content-Type" header        
+       });
+   
+         try {
+           const newData = await response.json();
+           console.log(newData);
+           return newData
+         }catch(error) {
+         console.log("i am error", error);
+         // appropriately handle the error
+         }
+     }
+   
+     const a = document.getElementById('generate').addEventListener('click',performAction);
+
+    function performAction(e){
+        const zip = document.getElementById("zip").value;
+        const feelings = document.getElementById("feelings").value;
+        const date = document.getElementById("mydate").value;
+        const temp = document.getElementById("temp").value;
+        const response = document.getElementById("response").value;
+
+        postData('/weatherDetails',{ Zip : zip,userFeelings :feelings,Date :date, Temperature:temp , userResponse:response })
+
+    }
+
+   /*GET DATA*/
+   
